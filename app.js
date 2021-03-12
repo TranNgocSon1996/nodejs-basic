fc22fb5acab62b73cb7599a228430da6c2e31d0b
TranNgocSon1996 <transon821996@gmail.com>
2021-03-12 15:35:49 +0700

    module fs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(request, response){
     response.writeHead(200,{'Content-Type': 'text/html'})
-    response.write('rep');
-    var param = request.url;
-    response.write(param);
-    response.end();
+    fs.readFile('index.html','utf8',function(err,data){
+        if (err) throw err;
+        response.write(data);
+        response.end();
+    })
 }).listen(8000);
